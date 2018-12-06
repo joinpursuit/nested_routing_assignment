97fbad75e4d6298f50d9360efd1906ccc664c03c
Reed Gaines <reed.gaines@gmail.com>
2018-12-06 10:54:24 -0500

    post assignment

---
+++ data.js
@@ -0,0 +1,133 @@
+let users = [
+  { id: 1, name: "Gregor Samsa", age: 54 },
+  { id: 2, name: "Bobbie Tillman", age: 23 },
+  { id: 3, name: "Tabitha Ealhstan", age: 34 },
+  { id: 4, name: "Kamal Ante", age: 46 },
+  { id: 5, name: "Keenan Kristian", age: 19 },
+  { id: 6, name: "Lynne Meztli", age: 44 },
+  { id: 7, name: "Amrita Gabriela", age: 62 },
+  { id: 8, name: "Daniel Elke", age: 78 },
+  { id: 9, name: "Roberta Alba", age: 27 },
+  { id: 10, name: "Julia Layla", age: 55 }
+];
+
+let posts = [
+  {
+    id: 1,
+    userId: 6,
+    body:
+      "Creative learning curve business plan. Conversion bandwidth partner network traction deployment focus leverage. Monetization social proof marketing scrum project investor innovator stock."
+  },
+  {
+    id: 2,
+    userId: 2,
+    body:
+      "Growth hacking twitter iPad assets beta holy grail prototype. Monetization accelerator supply chain social media prototype angel investor responsive web design focus investor churn rate business model canvas mass market android infrastructure."
+  },
+  {
+    id: 3,
+    userId: 4,
+    body:
+      "Freemium MVP graphical user interface bandwidth crowdfunding stealth. Deployment traction pitch innovator beta monetization iPhone pivot."
+  },
+  {
+    id: 4,
+    userId: 8,
+    body:
+      "Vesting period virality prototype focus interaction design infrastructure founders accelerator backing responsive web design bandwidth entrepreneur early adopters long tail. Termsheet focus scrum project innovator prototype. Social proof ownership influencer. Client marketing twitter partner network prototype customer iPad."
+  },
+  {
+    id: 5,
+    userId: 3,
+    body:
+      "Low hanging fruit ownership prototype stealth sales ecosystem stock infographic beta freemium."
+  },
+  {
+    id: 6,
+    userId: 1,
+    body:
+      "Ownership direct mailing hackathon handshake equity facebook strategy non-disclosure agreement leverage value proposition deployment incubator."
+  },
+  {
+    id: 7,
+    userId: 9,
+    body:
+      "Equity assets beta early adopters startup ownership iteration user experience android angel investor focus branding market success. Network effects deployment MVP hypotheses influencer startup social media."
+  },
+  {
+    id: 8,
+    userId: 10,
+    body:
+      "Monetization android strategy crowdfunding launch party infographic ecosystem value proposition agile development."
+  },
+  {
+    id: 9,
+    userId: 5,
+    body:
+      "A/B testing hypotheses ecosystem entrepreneur conversion interaction design. Seed money funding pitch."
+  },
+  { id: 10, userId: 7, body: "Disruptive value proposition metrics." }
+];
+
+let pictures = [
+  {
+    id: 1,
+    userId: 6,
+    url:
+      "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 2,
+    userId: 2,
+    url:
+      "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 3,
+    userId: 4,
+    url:
+      "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 4,
+    userId: 8,
+    url:
+      "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 5,
+    userId: 3,
+    url:
+      "https://images.pexels.com/photos/62321/kitten-cat-fluffy-cat-cute-62321.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 6,
+    userId: 1,
+    url:
+      "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 7,
+    userId: 9,
+    url:
+      "https://images.pexels.com/photos/257532/pexels-photo-257532.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 8,
+    userId: 10,
+    url:
+      "https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 9,
+    userId: 5,
+    url:
+      "https://images.pexels.com/photos/979247/pexels-photo-979247.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 10,
+    userId: 7,
+    url:
+      "https://images.pexels.com/photos/271955/pexels-photo-271955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }
+];
