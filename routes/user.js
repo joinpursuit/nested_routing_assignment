6a5473ef63284d31e95c229a13a8a5713636c2bb
RinatTr <rinatreg@gmail.com>
2018-12-06 16:25:21 -0500

    done

---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+let users = [
+  { id: 1, name: "Gregor Samsa", age: 54 },
+  { id: 2, name: "Bobbie Tillman", age: 23 },
+  { id: 3, name: "Tabitha Ealhstan", age: 34 },
+  { id: 4, name: "Kamal Ante", age: 46 },
+  { id: 5, name: "Keenan Kristian", age: 19 },
+  { id: 6, name: "Lynne Meztli", age: 44 },
+  { id: 7, name: "Amrita Gabriela", age: 62 },
+  { id: 8, name: "Daniel Elke", age: 78 },
+  { id: 9, name: "Roberta Alba", age: 27 },
+  { id: 10, name: "Julia Layla", age: 55 }
+]
+
+router.get('/', (req, res) => {
+  res.json({users: users})
+})
+
+router.get('/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+
+  let selectedUser = users.filter(userObj => {
+    return (userObj.id === id)
+  })
+
+  res.json(selectedUser[0])
+
+})
+
+module.exports = router
